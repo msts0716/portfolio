01ffa4165ef8df824c74a9c0ab43c3236ff54f94
msts0716 <myui70517@gmail.com>
2019-09-18 19:02:46 +0900

    htmlアップロード1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+/*DOM(htmlの後に読み込んでくださいという)コード*/
+	$('.toTop').on('click',function(){
+			$('html,body').animate({
+				scrollTop:0
+			},600);/*1000msで1秒、0.6sでも可*/
+			return false;
+	});
+})
+
+
+$(function(){
+/*DOM(htmlの後に読み込んでくださいという)コード*/
+	$('.about').on('click',function(){
+			$('html,body').animate({
+				scrollTop:740
+			},600);/*1000msで1秒、0.6sでも可*/
+			return false;
+	});
+})
+
+$(function(){
+/*DOM(htmlの後に読み込んでくださいという)コード*/
+	$('.works').on('click',function(){
+			$('html,body').animate({
+				scrollTop:1120
+			},600);/*1000msで1秒、0.6sでも可*/
+			return false;
+	});
+})
+
+$(function(){
+/*DOM(htmlの後に読み込んでくださいという)コード*/
+	$('.blog').on('click',function(){
+			$('html,body').animate({
+				scrollTop:2040
+			},600);/*1000msで1秒、0.6sでも可*/
+			return false;
+	});
+})
---
+++ js/strform.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('.chk').on('blur',function(){
+		var name = $(this).attr('name');
+				//直線にクリックした「これ」という形で指定が可能
+			if($(this).val()==''){
+						console.log(message)
+					$('.err_'+name).html('入力してください');//NULLチェック
+			}else{
+				var result = null;
+				var message = '';
+					
+				//入力値チェック
+				if(name=='phone'){//電話番号チェック
+					result = $(this).val().match(/^\d{10,11}$/);
+					message =　'電話番号の形式が異なります';
+				}else if(name=='mail'){//メールアドレスチェック
+					result = $(this).val().match(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9]{1,}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9_.-]{1,}$/);
+					message =　'メールアドレスの形式が異なります';
+				}else{		  //不正文字チェック
+					result = $(this).val().match(/[^!"#$%&'()\*\+\-\.,\/:;<=>?@\[\\\]^_`{|}~]+/);
+					message = '不正な記号が含まれています';
+				}
+					
+				if(result==null){//エラー出力及び削除処理
+					$('.err_'+name).html(message);
+				}else{
+					$('.err_'+name).html('');
+				}
+
+			}
+	});
+})
