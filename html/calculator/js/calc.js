06c037dac475da493b04a3253d3f8079de714018
msts0716 <myui70517@gmail.com>
2019-11-01 18:06:43 +0900

    電卓完了　ストップウォッチ追加

--- html/calculator/js/calc.js
+++ html/calculator/js/calc.js
@@ -6,11 +6,14 @@ $(function(){
 	var nextNum = 0;
 	var operator ='+';
 	var CalFlg = 0;
+	var DispFlg = 0;
+	//変数を作る際のポイント：全体向けは共通データとして保持したい時、個別向けは特定の機能の中で頻繫に出るものを簡略化するために用いる
+
 
 	//数字を入れる
 	$('.Num').on('click',function(){	
-		var inputData = $(this).val();
-		var dData = $('.dspData').val();
+		var inputData = $(this).val();//押した数字を格納する
+		var dData = $('.dspData').val();//現在表示されている数字を保存する
 		if (CalFlg == 1){
 			dData = '0';
 		}
@@ -20,7 +23,7 @@ $(function(){
 		}else{
 			dData += inputData;
 		}
-		$('.dspData').val(dData);
+		$('.dspData').val(dData);//dspDataにdDataの数字を出力する
 		CalFlg = 0;
 		$('.Clear').val('C');
 	});
@@ -28,6 +31,12 @@ $(function(){
 	//四則演算
 	$('.Comp').on('click',function(){
 		var ope = $(this).val();
+		
+		//初期表示時'='→計算ストップ
+		if (ope == '=' && DispFlg == 0) return;
+		//'='以外が押された→計算式稼働
+		if (ope != '=') DispFlg = 1;
+
 		if (CalFlg == 0) {
 			nextNum = $('.dspData').val();
 			//nextNumにdspDataに入っている値を取り出して格納する(取り込む)
@@ -38,7 +47,7 @@ $(function(){
 			$('.dspData').val(tData);
 			//dspDataにtDataの値を表示する
 		}else{
-			if (ope=='=') {	
+			if (ope=='=') {
 				var tData = inputNum + operator + nextNum;
 				tData = eval(tData);
 				//eval:文字式を式として計算する
@@ -46,6 +55,9 @@ $(function(){
 				$('.dspData').val(tData);
 			}
 		}
+		
+
+
 		if(ope=='÷'){
 			ope='/';
 		}
@@ -63,6 +75,7 @@ $(function(){
 	$('.Clear').on('click',function(){
 		var Mode = $(this).val();
 		if(Mode == 'AC'){
+			DispFlg = 0;
 			dspStart = '0';
 			$('.dspData').val(dspStart);
 			inputNum = 0;
@@ -80,8 +93,34 @@ $(function(){
 
 	//プラスマイナス
 	$('.PlusMinus').on('click',function(){
+		var dData = $('.dspData').val();
 		dData = (-1) * dData;
 		$('.dspData').val(dData);
+	});
+
+	//パーセント
+	$('.Percent').on('click',function(){
+		var dData = $('.dspData').val();
+		dData = dData / 100;
+		$('.dspData').val(dData);
+	});
+
+
+	//小数点
+	$('.Dot').on('click',function(){
+		var inputData = $(this).val();
+		var dData = $('.dspData').val();
+		if (CalFlg == 1){
+			dData = '0';
+		}
+		
+		CalFlg = 0;
+
+		//表示している数字にピリオドがある場合
+		if(dData.match(/\./)) return;
+		dData += inputData;
+		$('.dspData').val(dData);
+		$('.Clear').val('C');
 	});
 
 
