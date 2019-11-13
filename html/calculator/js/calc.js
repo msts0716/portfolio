ec1986386c21bf66a438ed196c8c7ea143c5d959
msts0716 <myui70517@gmail.com>
2019-11-13 22:13:38 +0900

    書式整理

--- html/calculator/js/calc.js
+++ html/calculator/js/calc.js
@@ -1,63 +1,46 @@
 $(function(){
 	var dspStart = '0';
 	$('.dspData').val(dspStart);
-	//dspDataにdspStartを取り込む
 	var inputNum = 0;
 	var nextNum = 0;
 	var operator ='+';
 	var CalFlg = 0;
 	var DispFlg = 0;
-	//変数を作る際のポイント：全体向けは共通データとして保持したい時、個別向けは特定の機能の中で頻繫に出るものを簡略化するために用いる
-
-
 	//数字を入れる
 	$('.Num').on('click',function(){	
-		var inputData = $(this).val();//押した数字を格納する
-		var dData = $('.dspData').val();//現在表示されている数字を保存する
+		var inputData = $(this).val();
+		var dData = $('.dspData').val();
 		if (CalFlg == 1){
 			dData = '0';
 		}
 		if(dData==='0'){
-			//＝が3つ:完全一致
 			dData = inputData;
 		}else{
 			dData += inputData;
 		}
-		$('.dspData').val(dData);//dspDataにdDataの数字を出力する
+		$('.dspData').val(dData);
 		CalFlg = 0;
 		$('.Clear').val('C');
 	});
-
 	//四則演算
 	$('.Comp').on('click',function(){
 		var ope = $(this).val();
-		
-		//初期表示時'='→計算ストップ
 		if (ope == '=' && DispFlg == 0) return;
-		//'='以外が押された→計算式稼働
 		if (ope != '=') DispFlg = 1;
-
 		if (CalFlg == 0) {
 			nextNum = $('.dspData').val();
-			//nextNumにdspDataに入っている値を取り出して格納する(取り込む)
 			var tData = inputNum + operator + nextNum;
 			tData = eval(tData);
-			//eval:文字式を式として計算する
 			inputNum = tData;
 			$('.dspData').val(tData);
-			//dspDataにtDataの値を表示する
 		}else{
 			if (ope=='=') {
 				var tData = inputNum + operator + nextNum;
 				tData = eval(tData);
-				//eval:文字式を式として計算する
 				inputNum = tData;
 				$('.dspData').val(tData);
 			}
 		}
-		
-
-
 		if(ope=='÷'){
 			ope='/';
 		}
@@ -65,12 +48,10 @@ $(function(){
 			ope='*';
 		}
 		if(ope!='='){
-			//opeが=ではないとき
 			operator=ope;
 		}
 		CalFlg = 1;
 	});
-
 	//オールクリアとクリア
 	$('.Clear').on('click',function(){
 		var Mode = $(this).val();
@@ -90,22 +71,18 @@ $(function(){
 			$('.Clear').val('AC');
 		}
 	});
-
 	//プラスマイナス
 	$('.PlusMinus').on('click',function(){
 		var dData = $('.dspData').val();
 		dData = (-1) * dData;
 		$('.dspData').val(dData);
 	});
-
 	//パーセント
 	$('.Percent').on('click',function(){
 		var dData = $('.dspData').val();
 		dData = dData / 100;
 		$('.dspData').val(dData);
 	});
-
-
 	//小数点
 	$('.Dot').on('click',function(){
 		var inputData = $(this).val();
@@ -113,10 +90,7 @@ $(function(){
 		if (CalFlg == 1){
 			dData = '0';
 		}
-		
 		CalFlg = 0;
-
-		//表示している数字にピリオドがある場合
 		if(dData.match(/\./)) return;
 		dData += inputData;
 		$('.dspData').val(dData);
--- html/stopwatch/js/stopwatch.js
+++ html/stopwatch/js/stopwatch.js
@@ -1,5 +1,4 @@
 $(function(){
-
 	//コンマ
 	var count = 0;
 	var DotCount = 0;
@@ -7,29 +6,21 @@ $(function(){
 	var Minute = 0;
 	var timerID;
 	var RapCount = 0;
-	//$('.DespStart').text(count);
-
 	var countup = function(){
 		count++;
-		//$('.DespStart').text(count);
 		Dot();
 	}
-
 	var SecUp = function(){
 		Seccond++;
 	}
-
 	var MinUp = function(){
 		Minute++;
 	}
-
 	//スタート&ストップ
 	$('.Start').on('click',function(){
 		var Mode = $(this).val();
 		if(Mode == '開 始'){
-			//$('.DespStart').text(timerID);
 			timerID = setInterval(countup,10); 
-			//1秒毎にcountup()を呼び出し
 			$('.Start').val('停 止');
 			$('.Rap').val('ラップ');
 		}else{
@@ -38,62 +29,43 @@ $(function(){
 			$('.Rap').val('クリア');
 		}
 	});
-
 	//ストップウォッチ
 	var Dot = function(){
-		//コンマ
 		DotCount = count;
 		if(String(DotCount).length>=2) {
-		//コンマ10の時はそのまま表示する
 			DotCount = String(DotCount).substr(-2);
 		}else{
-		//コンマ1秒の時には0をつける
 			DotCount = '0' + String(DotCount);
 		}
 		$('.Dot').text(DotCount);
-
-		//コンマを秒に切り替える
 		if(DotCount>=99) {
 			SecUp();
-			//1秒の時には0をつける
 			if(Seccond<=9) {
 				Seccond = '0' + String(Seccond);
-			//10秒の時はそのまま
 			}else{
 				Seccond = String(Seccond).substr(-4,2);
 			}
 		}
-
-		//0秒の時は00//
 		if(Seccond==0) {
 				Seccond = '00';
 			}
-
-
 
 		//秒を分に切り替える
 		if(Seccond>59) {
 			(Seccond)= 0;
 			MinUp();
-			//1分の時には0をつける
 			if(Minute<=9) {
 				Minute = '0' + String(Minute);
-			//10分の時はそのまま
 			}else{
 				Minute = String(Minute).substr(-6,2);
 			}
 		}
-
-		//0秒の時は00//
 		if(Minute==0) {
 				Minute = '00';
 			}
-
 		$('.Seccond').text(Seccond);
 		$('.Minute').text(Minute);
 	}
-
-
 	//ラップ
 	$('.Rap').on('click',function(){
 		var Mode = $(this).val();
@@ -111,11 +83,6 @@ $(function(){
 			$('.Dot').text('00');
 			$('.Seccond').text('00');
 			$('.Minute').text('00');
-		}
-		
-		
+		}	
 	});
-
-
-
 });
